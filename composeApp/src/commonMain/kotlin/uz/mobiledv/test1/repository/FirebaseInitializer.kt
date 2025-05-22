7b14b94e9f5d22faa77764c08771c5061e37d9a8
MobileDv <jahongirmirzotolqinov6@gmail.com>
2025-05-22 20:19:51 +0500

    Remove FirebaseInitializer
    This commit removes the `FirebaseInitializer` object and its platform-specific implementations. The Firebase initialization logic is no longer needed.

--- composeApp/src/commonMain/kotlin/uz/mobiledv/test1/repository/FirebaseInitializer.kt
+++
@@ -1,7 +0,0 @@
-package uz.mobiledv.test1.repository
-
-// Common expect declaration for Firebase initialization
-expect object FirebaseInitializer{
-    fun initialize()
-    fun getAppContext(): Any? // To pass context if needed, more relevant for Android
-}
